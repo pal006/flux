eb73628cb126197a621cf521a282aae8dad68a23
Gopala Raju <gopala.raju@jdpower.com.sg>
2015-12-01 18:16:21 +0800

    Updated to use solr 5.3 libraries. - only basic testing is completed.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.codesignals/flux "0.6.0"
+(defproject com.codesignals/flux "0.7.0"
   :description "A clojure client library for Solr"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.solr/solr-core "4.9.0"]
-                 [org.apache.solr/solr-solrj "4.9.0"]
+                 [org.apache.solr/solr-core "5.3.0"]
+                 [org.apache.solr/solr-solrj "5.3.0"]
                  [javax.servlet/servlet-api "2.5"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [org.slf4j/slf4j-log4j12 "1.7.7"]
