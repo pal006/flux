23c7cddc976699fab041bf4ff185737e3cc0d7a6
Gopala Raju <gopala.raju@jdpower.com.sg>
2016-07-22 16:46:04 +0800

    Added load balanced http connection

--- src/flux/http.clj
+++ src/flux/http.clj
@@ -1,5 +1,9 @@
 (ns flux.http
-  (import [org.apache.solr.client.solrj.impl HttpSolrServer]))
+  (import [org.apache.solr.client.solrj.impl HttpSolrServer LBHttpSolrServer]))
 
 (defn create [base-url core-name]
   (HttpSolrServer. (str base-url "/" (name core-name))))
+
+(defn create-lb-conn [base-urls]
+  (LBHttpSolrServer. base-urls))
+
